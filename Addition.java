90f99c512492e016b498825e78522a1ab7b6be47
daniel-ericson <danielericson2016@gmail.com>
2024-08-15 09:15:55 -0300

    Addition-traduzido

--- Addition.java
+++ Addition.java
@@ -7,9 +7,9 @@ public class Addition // cria a classe de adição
    {
       // Obtem a entrada do usuario atravez da caixa de diálogo que foi criada em JOptinPane
       String firstNumber = // prepara a primeira classe para receber o primeiro número 
-         JOptionPane.showInputDialog("Enter first integer"); // exibe uma caixa de diálogo onde solicita para digigitar o primeiro número inteiro, alem de mostrar o botçao de "ok" e "cancel"
+         JOptionPane.showInputDialog("Digite o primeiro número"); // exibe uma caixa de diálogo onde solicita para digigitar o primeiro número inteiro, alem de mostrar o botçao de "ok" e "cancel"
       String secondNumber =// prepara a segunda classe para receber o segundo o número 
-         JOptionPane.showInputDialog("Enter second integer"); // exibe uma caixa de diálogo onde solicita para digigitar o segundo número inteiro, alem de mostrar o botçao de "ok" e "cancel"
+         JOptionPane.showInputDialog("Digite o segundo número"); // exibe uma caixa de diálogo onde solicita para digigitar o segundo número inteiro, alem de mostrar o botçao de "ok" e "cancel"
 
          // converte as entradas de texto em resultados
       int number1 = Integer.parseInt(firstNumber); // converte a primeira entreada de texto no em um número inteiro
@@ -18,7 +18,7 @@ public class Addition // cria a classe de adição
       int sum = number1 + number2; // realiza o calculo(soma) dos dois numeros inteiros digitados na caisxa de entrada 
 
       //exibe  o resultado da soma realixada 
-      JOptionPane.showMessageDialog(null, "The sum is " + sum, // exibe o resultado da soma  
-         "Sum of Two Integers", JOptionPane.PLAIN_MESSAGE); // mostra a uma janela com o texto 
+      JOptionPane.showMessageDialog(null, "A soma dos números é:" + sum, // exibe o resultado da soma  
+         "Soma de dois inteiros", JOptionPane.PLAIN_MESSAGE); // mostra a uma janela com o texto 
    } 
 } // fim do código de adição
